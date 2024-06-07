1fae65d1a27eb4f6a2417d5686c3dbaf67af5ff8
manish-eg <makul@eg.dk>
2024-06-07 10:38:53 +0530

    Name added

--- GitHubMainApplication/Program.cs
+++ GitHubMainApplication/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubMainApplication
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+            Console.WriteLine("Manish");
         }
     }
 }
