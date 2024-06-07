f8639860e17d2902964548cac5030f17a8993bd6
manish-eg <makul@eg.dk>
2024-06-07 14:54:25 +0530

    class and function created to find the maximum and minimum value of the array

---
+++ GitHubMainApplication/Min_Max_Value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GitHubMainApplication
+{
+    internal class Min_Max_Value
+    {
+        public void findMinMaxValue(int[] numberArray)
+        {
+            int maxNumberInArray = numberArray[0];
+            int minNumberInArray = numberArray[0];
+
+            for (int iterator = 1; iterator < numberArray.Length; iterator++)
+            {
+                if (maxNumberInArray < numberArray[iterator])
+                    maxNumberInArray = numberArray[iterator];
+                if (minNumberInArray > minNumberInArray)
+                    minNumberInArray = numberArray[iterator];
+            }
+            Console.WriteLine($"The maximum value in the array is {maxNumberInArray}");
+            Console.WriteLine($"The minimum value in the array is {minNumberInArray}");
+        }
+    }
+}
